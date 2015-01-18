ab559e4dc8e3eac9767f33b01608b5394a9fff82
root <root@localhost.localdomain>
2015-01-18 20:36:05 +0000

    initial setup

---
+++ administrator/components/com_admintools/version.php
@@ -0,0 +1,7 @@
+<?php
+// Protect from unauthorized access
+defined('_JEXEC') or die;
+
+define('ADMINTOOLS_VERSION', '3.4.3');
+define('ADMINTOOLS_DATE', '2015-01-11');
+define('ADMINTOOLS_PRO','0');
